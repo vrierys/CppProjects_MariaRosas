c97d47b44769163645ff0e88ac84972d8c6801c1
mariarosasvalsera <mariarosasvalsera@S1-14>
2017-03-02 09:05:28 +0100

    Random EnemyGenerator arreglado.

--- Random EnemyGenerator/Random EnemyGenerator.cpp
+++ Random EnemyGenerator/Random EnemyGenerator.cpp
@@ -1,86 +1,97 @@
 // Random EnemyGenerator.cpp : Defines the entry point for the console application.
 //
 
-#include "stdafx.h"
-#include  <ctime>
 #include <iostream>
 #include <string>
-using namespace std;
+#include <ctime>
 
-
-enum class EnemyType { ZOMBIE, VAMPIRE, GHOST, WEREWOLF, MAX };
+enum class EnemyType { ZOMBIE, VAMPIRE, GHOST, WITCH, MAX };
 
 struct Enemy {
+public:
 
 	EnemyType type;
-	string name;
+	std::string name;
 	int health;
+};
 
-	string getEnemyTypeString() {
+bool dechu(Enemy a, Enemy b) {
 
-		switch (type) {
-		case EnemyType::ZOMBIE: return "zombie";
-		case EnemyType::VAMPIRE: return "vampire";
-		case EnemyType::GHOST: return "ghost";
-		case EnemyType::WEREWOLF: return "werewolf";
-		default: return "";
-		}
+	return (a.type == b.type && a.name == b.name);
+
+}
+
+Enemy CreateRandomEnemy() {
+
+	Enemy enemigo;
+
+	enemigo.type = static_cast <EnemyType>(rand() % static_cast<int>(EnemyType::MAX));
+
+	int name1 = rand() % 5;
+
+	switch (name1) {
+	case 0: enemigo.name = "Ramon";
+		break;
+	case 1: enemigo.name = "Marc";
+		break;
+	case 2: enemigo.name = "David";
+		break;
+	case 3: enemigo.name = "Albert";
+		break;
+	case 4: enemigo.name = "Alex";
+		break;
 	}
+	enemigo.health = rand() % 101;
+
+
+	return enemigo;
 
 };
 
-bool operator==(const Enemy &a, const Enemy &b) {
 
-	return a.name == b.name && a.type == b.type;
+std::string GetEnemyTypeString(EnemyType a) {
+
+	switch (a) {
+	case EnemyType::ZOMBIE: return "Zombie";
+		break;
+	case EnemyType::VAMPIRE: return "Vampire";
+		break;
+	case EnemyType::GHOST: return "Ghost";
+		break;
+	case EnemyType::WITCH: return "Witch";
+		break;
+	}
+
 }
 
-static Enemy CreateRandomEnemy() {
 
-	static const int MAX_LIFE{ 500 };
-	static const string NAMES[]{
-		"Gertrudiz",
-		"Pancracia",
-		"Anacleto",
-		"Hipolito",
-		"Eustaquio",
-		"Fulgencia"
-	};
+void main() {
 
-	return Enemy{
-		static_cast<EnemyType>(rand() % static_cast<int>(EnemyType::MAX)),
-		NAMES[rand() % (sizeof NAMES / sizeof string)],
-		rand() % MAX_LIFE
-	};
-}
+	srand(time(nullptr));
 
-int main()
-{
+	const int MAX_ENEMIES = 5;
 
-	srand(static_cast<unsigned>(time(nullptr)));
-	const int MAX_ENEMIES{ 5 };
-	Enemy enemies[MAX_ENEMIES];
-	{
-		int i{ 0 };
-		while (i < MAX_ENEMIES) {
+	static Enemy arr[MAX_ENEMIES];
 
-			enemies[i] = CreateRandomEnemy();
-			int j{ i - 1 };
-			while (j >= 0) {
+	for (int i = 0; i < 6; i++) {
 
-				if (enemies[i] == enemies[j]) {
-					--i;
-					break;
-				}
-				j--;
+		arr[i] = CreateRandomEnemy();
+
+		for (int j = 0; j < i; j++) {
+
+			if (dechu(arr[i], arr[j]) == true) {
+				i--;
 			}
-			i++;
 		}
 	}
 
-	cout << "List of enemies:\n";
-	for (auto &enemy : enemies) {
-		cout << enemy.name << "is a" << enemy.getEnemyTypeString() << "whose life is" << enemy.health << endl;
+	for (int w = 0; w < 6; w++) {
+
+		std::string chan = GetEnemyTypeString(arr[w].type);
+
+		std::cout << chan << " " << arr[w].name << " " << arr[w].health << std::endl;
+
 	}
 
-	return 0;
+
 }
